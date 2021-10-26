f4c082db10faf5d67ffcb51fc3d761139b48aa05
dwhnx <hellofuran@gmail.com>
2021-10-26 14:28:23 -0400

    initial commit

---
+++ src/js/scripts.js
@@ -0,0 +1,101 @@
+// store the link plus the API key in a variable
+const key = " replace with my own ";
+const API = `https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=${key}`;
+const storagePrefix = "nyt-autosave";
+
+// import { setWithExpiry, getWithExpiry } from "./modules/localStorageHelpers.js";
+
+function setWithExpiry(key, value, ttl) {
+  const now = new Date();
+  // `item` is an object which contains the original value
+  // as well as the time when it's supposed to expire
+  const item = {
+    value: value,
+    expiry: now.getTime() + ttl,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+function getWithExpiry(key) {
+  const itemStr = localStorage.getItem(key);
+  // if the item doesn't exist, return null
+  if (!itemStr) {
+    console.log("no item string");
+    return null;
+  }
+  console.log("item string found!", itemStr);
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  // compare the expiry time of the item with the current time
+  if (now.getTime() > item.expiry) {
+    // If the item is expired, delete the item from storage
+    // and return null
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+}
+
+function getStories() {
+  const value = getWithExpiry(storagePrefix);
+  if (!value) {
+    console.log(" expired - fetching again ");
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => showData(data.results));
+  } else {
+    document.querySelector(".stories").innerHTML = value;
+  }
+}
+
+function showData(stories) {
+  const looped = stories
+    .map(
+      (story) => `
+    <div class="item">
+    <img src="${story.multimedia ? story.multimedia[2].url : ""}" alt="${
+        story.multimedia ? story.multimedia[2]?.caption : ""
+      }" />
+    <figcaption>${
+      story.multimedia ? story.multimedia[2]?.caption : ""
+    }</figcaption>
+      <h3><a href="${story.url}">${story.title}</a></h3>
+      <p>${story.abstract}</p>
+    </div>
+  `
+    )
+    .join("");
+
+  document.querySelector(".stories").innerHTML = looped;
+  setWithExpiry(storagePrefix, looped, 1000 * 60);
+}
+
+if (document.querySelector(".home")) {
+  getStories();
+}
+
+document.addEventListener("click", clickHandlers);
+
+function clickHandlers(event) {
+  if (event.target.matches("#pull")) {
+    showMenu();
+    event.preventDefault();
+  }
+  if (event.target.matches(".content-video a")) {
+    videoSwitch();
+    event.preventDefault();
+  }
+}
+
+function showMenu() {
+  document.querySelector("body").classList.toggle("show-nav");
+}
+
+function videoSwitch(event) {
+  const iFrame = document.querySelector("iframe");
+  const videoLinks = document.querySelectorAll(".content-video a");
+  videoLinks.forEach((videoLink) => videoLink.classList.remove("active"));
+  event.target.classList.add("active");
+  const videoToPlay = event.target.getAttribute("href");
+  iFrame.setAttribute("src", videoToPlay);
+}
---
+++ src/pages/home.md
@@ -0,0 +1,11 @@
+---
+pageTitle: Home
+navTitle: Home
+pageClass: home
+date: 2021-10-21
+permalink: /
+---
+
+## Articles
+
+<div class="stories">Loading...</div>
